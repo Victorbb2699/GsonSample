5f50df7e28127df511db54d1953f68c4cb3423fd
Victor Bethencourt <victolo500@gmail.com>
2019-11-30 13:17:45 +0000

    update

--- src/main/java/gsonMaven/main.java
+++ src/main/java/gsonMaven/main.java
@@ -1,4 +1,5 @@
 package gsonMaven;
+
 import java.util.Scanner;
 
 import com.google.gson.Gson;
@@ -6,27 +7,27 @@ import com.google.gson.GsonBuilder;
 
 import gsonMaven.Persona;
 
-public class main {
+public class Main {
 
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
-		
+
 		Persona p = new Persona();
-		
+
 		System.out.println("Introduce una persona.");
 		System.out.println("Nombre:  ");
 		p.setNombre(entrada.nextLine());
 		System.out.println("Apellidos:  ");
-		p.setApellidos(entrada.nextLine()); 
+		p.setApellidos(entrada.nextLine());
 		System.out.println("Edad:  ");
 		p.setEdad(entrada.nextInt());
-		
+
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		String json = gson.toJson(p);
 		System.out.println(json);
-	
+
 		entrada.close();
-		
+
 	}
 
 }
